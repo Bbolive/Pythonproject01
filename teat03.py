29cbcb0ff87f7d62f78a09cee793f44db73ae03d
Bbolive <rapatsara.bam@gmail.com>
2023-08-18 18:01:49 +0700

    code V.1 : Bbolive

---
+++ teat03.py
@@ -0,0 +1,20 @@
+print('AAA'); print('BBB')
+#--------------------------------
+print('CCC')
+#----------------------------
+print('DDD\nEEE\nFFF')
+print('DDD\tEEE\tFFF')
+#-----------------------------
+print("welcom 'SAU'")
+print('welcom "LOT"')
+#--------------------------
+print("welcom \"lot\"")
+print('welcom \'lot\'')
+print('ABC\\DEF')
+#--------------------------
+print('''
+    Hi...
+    IOT...
+    SAU...
+    WOW...
+    ''')
---
+++ test02.py
@@ -0,0 +1,8 @@
+print("Hi",end='')                 #string
+print('Hello',end='')              #Str
+print('Hello World!')       #str
+print(445556)               #int
+print('false')              #str
+print(True)                 #boolean
+print(10+10+20)             #ex
+
